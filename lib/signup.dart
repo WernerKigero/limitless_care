af4e418534004f4dcb1b2f46ce75058f9c831546
WernerKigero <grobler.werner671@gmail.com>
2024-01-04 01:50:37 +0200

    service provider sign Up complete and added font awesome package also i made sign up pages scrollable

--- lib/signup.dart
+++ lib/signup.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import './main.dart';
 import 'llcTheme.dart'; // Import the custom theme
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Signup extends StatelessWidget {
   @override
@@ -60,15 +61,15 @@ class _SignUpState extends State<SignUp> {
   }
 
   Widget _buildUserPage() {
-    return Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage('assets/background_image.png'),
-          fit: BoxFit.cover,
+    return SingleChildScrollView(
+      child: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/background_image.png'),
+            fit: BoxFit.cover,
+          ),
         ),
-      ),
-      padding: EdgeInsets.all(16.0),
-      child: SingleChildScrollView(
+        padding: EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
@@ -144,28 +145,70 @@ class _SignUpState extends State<SignUp> {
   }
 
   Widget _buildServiceProviderPage() {
-    return Container(
-      // Add your service provider input fields here
-      child: Center(
-        child: Column( // Use a Column to arrange widgets vertically
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Thank you for choosing to render your service available on Limitless care, and for taking part in making day to day life more bareable. Please fill in the folowing details to sign up as a service provider',
-              style: TextStyle(fontSize: 18.0),
-            ),
-            SizedBox(height: 16.0), // SizedBox wrapped around the Text widget
-            _buildTextField("Email", Icons.email),
-            _buildTextField("Username", Icons.person),
-            _buildTextField("Password", Icons.lock),
-            _buildTextField("Confirm Password", Icons.lock),
-            _buildTextField("Number of caregivers available", Icons.numbers_sharp),
-          ],
+    return SingleChildScrollView(
+      child: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Thank you for choosing to render your service available on Limitless care, and for taking part in making day to day life more bearable. Please fill in the following details to sign up as a service provider',
+                style: TextStyle(fontSize: 18.0),
+              ),
+              SizedBox(height: 16.0),
+              _buildTextField("Email", Icons.email),
+              _buildTextField("Company Name", Icons.abc_sharp),
+              _buildTextField("Company Registration Number", FontAwesomeIcons.idCard),
+              _buildTextField("Address", FontAwesomeIcons.locationPin),
+              _buildTextField("Number of caregivers available", FontAwesomeIcons.hashtag),
+              _buildTextField("Username", Icons.person),
+              _buildTextField("Password", Icons.lock),
+              _buildTextField("Confirm Password", Icons.lock),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(
+                      AppTheme.llcTheme.primaryColor),
+                  padding: MaterialStateProperty.all(
+                      EdgeInsets.symmetric(horizontal: 20, vertical: 12)),
+                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(24.0),
+                  )),
+                  textStyle: MaterialStateProperty.all(TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  )),
+                ),
+                onPressed: () {
+                  _showSignUpToast(); // Call the function to show the toast
+                  // Add your signup logic here
+                },
+                child: Text('Sign Up'),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 
+  void _showSignUpToast() {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Row(
+          children: [
+            Icon(Icons.check, color: Colors.green),
+            SizedBox(width: 8.0),
+            Text('Sign Up successful for Service Provider',
+                style: TextStyle(color: Colors.green)),
+          ],
+        ),
+        backgroundColor: Colors.black,
+        duration: Duration(seconds: 3),
+      ),
+    );
+  }
 
   Widget _buildTextField(String label, IconData icon) {
     return Container(
